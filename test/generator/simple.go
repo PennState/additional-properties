06af2066844c657f39f01f1366844d51b9c3a56e
Steve Moyer <smoyer1@selesy.com>
2019-08-14 17:46:47 -0400

    Adds example Simple object for testing

--- test/generator/simple.go
+++ test/generator/simple.go
@@ -1,6 +1,18 @@
 package acceptance
 
+// Simple is the most basic struct to which we can add additional-
+// properties.
 type Simple struct {
 	A  string
 	AP map[string]interface{} `json:"*"`
+}
+
+func newTestSimple() Simple {
+	return Simple{
+		A: "Field A",
+		AP: map[string]interface{}{
+			"b": "Field B",
+			"c": "Field C",
+		},
+	}
 }
