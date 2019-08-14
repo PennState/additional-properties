3595f73fb2692f55a317b394d69fb94e8a0f0318
Steve Moyer <smoyer1@selesy.com>
2019-08-14 17:48:16 -0400

    Adds testing of generated Marshaler implementation

--- test/generator/generator_test.go
+++ test/generator/generator_test.go
@@ -5,17 +5,28 @@ import (
 	"io/ioutil"
 	"testing"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func Test(t *testing.T) {
+func TestGeneratedMarshalerWorks(t *testing.T) {
+	expected, err := ioutil.ReadFile("simple.json")
+	require.NoError(t, err)
+	input := newTestSimple()
+	data, err := json.Marshal(&input)
+	assert.NoError(t, err)
+	actual := []byte("{\"a\":\"Field A\",")
+	for _, b := range data[1:] {
+		actual = append(actual, b)
+	}
+	assert.Equal(t, expected, actual)
+}
+
+func TestGeneratedUnmarshalerWorks(t *testing.T) {
 	data, err := ioutil.ReadFile("simple.json")
 	require.NoError(t, err)
 	var s Simple
 	err = json.Unmarshal(data, &s)
 	assert.NoError(t, err)
-	log.Info("Simple: ", s)
-	assert.True(t, false)
+	assert.Equal(t, newTestSimple(), s)
 }
